963a0f1cb96810b16a63909564f3402610fbb627
Tim <tim@wizmacau.com>
2016-08-30 15:14:04 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var express = require('express')
 var request = require('request')
 var bodyParser = require('body-parser')
-
+var stream = require("stream")
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -22,7 +22,8 @@ app.post('/horn/:id', function(req, res) {
   }));
 })
 
-var textRawParser = bodyParser.text({type: 'text/plain'});
+var textRawParser = bodyParser.text();
+var jsonParser = bodyParser.json();
 
 app.post('/test-horn/:id', textRawParser , function(req, res) {
   var id = req.params.id;
@@ -41,6 +42,36 @@ app.post('/test-horn/:id', textRawParser , function(req, res) {
         res.send(raw)
     })
   )
+})
+
+app.post('/json-horn/:id', textRawParser , function(req, res) {
+  var id = req.params.id;
+  var url = "https://integram.org/" + id;
+  var raw = req.body;
+  var json = JSON.parse(raw);
+  //console.log(json);
+  var pre;
+  if(json.level=='error'){
+    pre = "👺 "
+  }else if( json.level=='info' ){
+    pre = "⭐ "
+  }else{
+    pre = "👻 " + `[${json.level}]`
+  }
+  var type = json.level;
+  var msg = json.message;
+  var post_json = JSON.stringify({text:`${pre} ${msg} \n ${json.url}`});
+  //console.log(post_json);
+  var a = new stream.PassThrough()
+  a.write(post_json)
+
+  a.pipe(
+    request.post({ url: url},function (error, response, body) {
+        res.statusCode = error ? 500 : 200;
+        res.send(post_json)
+    })
+  )
+  a.end()
 })
 
 
